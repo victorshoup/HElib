467e0d49bdeba25aaec648181516fd31cbd1cf5d
victorshoup <shoup@cs.nyu.edu>
2019-06-17 14:52:26 -0400

    .

--- src/NumbTh.cpp
+++ src/NumbTh.cpp
@@ -9,6 +9,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License. See accompanying LICENSE file.
  */
+
 #include "NumbTh.h"
 
 #include "timing.h"
